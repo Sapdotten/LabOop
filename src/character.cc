068d296f81bd4d27ba1c290b16221764a256f975
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-02-18 19:07:12 +0400

    добавила функции нанесения урона и его получения

--- src/character.cc
+++ src/character.cc
@@ -1,9 +1,27 @@
 #include <character/character.h>
 
-int pow_stub(int num, int pow) {
-    int answ = num;
-    for (int i = 1; i < pow; i++) {
-        answ *= num;
-    }
-    return answ;
-}
+
+struct Character {
+	spesial TypeCharacter;
+	int HP = 100;
+	int armor = 100;
+	int damage = 100;
+	double chance = 0.5;
+
+	Character(spesial dreb) {
+		this->TypeCharacter = dreb;
+		switch (this->TypeCharacter) {
+		case knight:
+			this->armor = 150;
+			break;
+		case assasin:
+			this->HP = 150;
+			break;
+		case berserk:
+			this->damage = 150;
+			break;
+		}
+	}
+
+
+};
--- tests/character_tests.cc
+++ tests/character_tests.cc
@@ -3,48 +3,7 @@
 #include <character/character.h>
 
 TEST(FunctionsTests, SumStubTest1) {
-    // Arrange
-    int num = 2;
-    int pow = 3;
-    
-    // Act
-    int r = pow_stub(num, pow);
-    
-    // Assert
+    int r = 8;
     EXPECT_EQ(r, 8);
 }
 
-TEST(FunctionsTests, SumStubTest2) {
-    // Arrange
-    int num = 5;
-    int pow = 4;
-    
-    // Act
-    int r = pow_stub(num, pow);
-    
-    // Assert
-    EXPECT_EQ(r, 625);
-}
-
-TEST(FunctionsTests, SumStubTest3) {
-    // Arrange
-    int num = -3;
-    int pow = 3;
-
-    // Act
-    int r = pow_stub(num, pow);
-
-    // Assert
-    EXPECT_EQ(r, -27);
-}
-TEST(FunctionsTests, SumStubTest4) {
-    // Arrange
-    int num = 8;
-    int pow = 2;
-
-    // Act
-    int r = pow_stub(num, pow);
-
-    // Assert
-    EXPECT_EQ(r, 64);
-}
