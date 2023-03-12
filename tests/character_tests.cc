ec6e310a033f075d1ce2e25263c312cbfcd7fde9
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-03-12 23:27:18 +0400

    Добавила тестовые значения

--- tests/character_tests.cc
+++ tests/character_tests.cc
@@ -30,13 +30,13 @@ TEST(CharacterTests, characterTest11) {//berserk without skill vs everybody(0) w
     int HPB = Berserk2.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 620);
+    EXPECT_EQ(HPB, 570);
+    EXPECT_EQ(HPA, 770);
 
     EXPECT_EQ(bk, 70);
     EXPECT_EQ(ba, 95);
-    EXPECT_EQ(Bb, 335);
+    EXPECT_EQ(bb, 95);
     EXPECT_EQ(Bk, 310);
     EXPECT_EQ(Ba, 335);
     EXPECT_EQ(Bb, 335);
@@ -67,13 +67,13 @@ TEST(CharacterTests, characterTest12) {//berserk without skill vs everybody(1) w
     int HPB = Berserk2.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 810);
+    EXPECT_EQ(HPB, 570);
+    EXPECT_EQ(HPA, 770);
 
     EXPECT_EQ(bk, 35);
     EXPECT_EQ(ba, 95);
-    EXPECT_EQ(Bb, 335);
+    EXPECT_EQ(bb, 95);
     EXPECT_EQ(Bk, 155);
     EXPECT_EQ(Ba, 335);
     EXPECT_EQ(Bb, 335);
@@ -108,16 +108,16 @@ TEST(CharacterTests, characterTest13) {//berserk without skill vs everybody(0) w
     int HPB = Berserk2.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 660);
+    EXPECT_EQ(HPB, 530);
+    EXPECT_EQ(HPA, 1200);
 
-    EXPECT_EQ(bk, 40);
+    EXPECT_EQ(bk, 50);
     EXPECT_EQ(ba, 0);
-    EXPECT_EQ(Bb, 440);
-    EXPECT_EQ(Bk, 340);
+    EXPECT_EQ(bb, 115);
+    EXPECT_EQ(Bk, 290);
     EXPECT_EQ(Ba, 0);
-    EXPECT_EQ(Bb, 440);
+    EXPECT_EQ(Bb, 355);
 }
 
 TEST(CharacterTests, characterTest14) {//berserk without skill vs everybody(1) with skill
@@ -149,16 +149,16 @@ TEST(CharacterTests, characterTest14) {//berserk without skill vs everybody(1) w
     int HPB = Berserk2.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 830);
+    EXPECT_EQ(HPB, 530);
+    EXPECT_EQ(HPA, 1200);
 
-    EXPECT_EQ(bk, 20);
+    EXPECT_EQ(bk, 25);
     EXPECT_EQ(ba, 0);
-    EXPECT_EQ(Bb, 440);
-    EXPECT_EQ(Bk, 170);
+    EXPECT_EQ(bb, 115);
+    EXPECT_EQ(Bk, 145);
     EXPECT_EQ(Ba, 0);
-    EXPECT_EQ(Bb, 440);
+    EXPECT_EQ(Bb, 355);
 }
 
 TEST(CharacterTests, characterTest15) {//berserk with skill vs everybody(0) without skill
@@ -192,16 +192,16 @@ TEST(CharacterTests, characterTest15) {//berserk with skill vs everybody(0) with
     int HPB = Berserk2.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 540);
+    EXPECT_EQ(HPB, 490);
+    EXPECT_EQ(HPA, 690);
 
-    EXPECT_EQ(bk, 110);
-    EXPECT_EQ(ba, 130);
-    EXPECT_EQ(Bb, 490);
-    EXPECT_EQ(Bk, 470);
-    EXPECT_EQ(Ba, 490);
-    EXPECT_EQ(Bb, 490);
+    EXPECT_EQ(bk, 90);
+    EXPECT_EQ(ba, 115);
+    EXPECT_EQ(bb, 115);
+    EXPECT_EQ(Bk, 370);
+    EXPECT_EQ(Ba, 395);
+    EXPECT_EQ(Bb, 395);
 }
 
 TEST(CharacterTests, characterTest16) {//berserk with skill vs everybody(1) without skill
@@ -235,16 +235,16 @@ TEST(CharacterTests, characterTest16) {//berserk with skill vs everybody(1) with
     int HPB = Berserk2.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 770);
+    EXPECT_EQ(HPB, 490);
+    EXPECT_EQ(HPA, 690);
 
-    EXPECT_EQ(bk, 55);
-    EXPECT_EQ(ba, 130);
-    EXPECT_EQ(Bb, 490);
-    EXPECT_EQ(Bk, 235);
-    EXPECT_EQ(Ba, 490);
-    EXPECT_EQ(Bb, 490);
+    EXPECT_EQ(bk, 45);
+    EXPECT_EQ(ba, 115);
+    EXPECT_EQ(bb, 115);
+    EXPECT_EQ(Bk, 185);
+    EXPECT_EQ(Ba, 395);
+    EXPECT_EQ(Bb, 395);
 }
 
 TEST(CharacterTests, characterTest17) {//berserk with skill vs everybody(0) with skill
@@ -282,16 +282,16 @@ TEST(CharacterTests, characterTest17) {//berserk with skill vs everybody(0) with
     int HPB = Berserk2.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 580);
+    EXPECT_EQ(HPB, 450);
+    EXPECT_EQ(HPA, 1200);
 
     EXPECT_EQ(bk, 70);
     EXPECT_EQ(ba, 0);
-    EXPECT_EQ(Bb, 530);
-    EXPECT_EQ(Bk, 430);
+    EXPECT_EQ(bb, 135);
+    EXPECT_EQ(Bk, 350);
     EXPECT_EQ(Ba, 0);
-    EXPECT_EQ(Bb, 530);
+    EXPECT_EQ(Bb, 415);
 }
 
 TEST(CharacterTests, characterTest18) {//berserk with skill vs everybody(1) with skill
@@ -329,16 +329,16 @@ TEST(CharacterTests, characterTest18) {//berserk with skill vs everybody(1) with
     int HPB = Berserk2.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 790);
+    EXPECT_EQ(HPB, 450);
+    EXPECT_EQ(HPA, 1200);
 
     EXPECT_EQ(bk, 35);
     EXPECT_EQ(ba, 0);
-    EXPECT_EQ(Bb, 530);
-    EXPECT_EQ(Bk, 215);
+    EXPECT_EQ(bb, 135);
+    EXPECT_EQ(Bk, 175);
     EXPECT_EQ(Ba, 0);
-    EXPECT_EQ(Bb, 530);
+    EXPECT_EQ(Bb, 415);
 }
 
 
@@ -369,16 +369,16 @@ TEST(CharacterTests, characterTest21) {//knight without skill vs everybody(0) wi
     int HPB = Berserk.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 900);
+    EXPECT_EQ(HPB, 850);
+    EXPECT_EQ(HPA, 1050);
 
-    EXPECT_EQ(kb, 50);
-    EXPECT_EQ(ka, 50);
-    EXPECT_EQ(kk, 30);
-    EXPECT_EQ(Kb, 50);
-    EXPECT_EQ(Ka, 50);
-    EXPECT_EQ(Kk, 30);
+    EXPECT_EQ(kb, 75);
+    EXPECT_EQ(ka, 75);
+    EXPECT_EQ(kk, 50);
+    EXPECT_EQ(Kb, 75);
+    EXPECT_EQ(Ka, 75);
+    EXPECT_EQ(Kk, 50);
 }
 
 TEST(CharacterTests, characterTest22) {//knight without skill vs everybody(1) without skill
@@ -406,16 +406,16 @@ TEST(CharacterTests, characterTest22) {//knight without skill vs everybody(1) wi
     int HPB = Berserk.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 950);
+    EXPECT_EQ(HPB, 850);
+    EXPECT_EQ(HPA, 1050);
 
-    EXPECT_EQ(kb, 50);
-    EXPECT_EQ(ka, 50);
-    EXPECT_EQ(kk, 15);
-    EXPECT_EQ(Kb, 50);
-    EXPECT_EQ(Ka, 50);
-    EXPECT_EQ(Kk, 15);
+    EXPECT_EQ(kb, 75);
+    EXPECT_EQ(ka, 75);
+    EXPECT_EQ(kk, 25);
+    EXPECT_EQ(Kb, 75);
+    EXPECT_EQ(Ka, 75);
+    EXPECT_EQ(Kk, 25);
 }
 
 TEST(CharacterTests, characterTest23) {//knight with skill vs everybody(0) without skill
@@ -449,16 +449,16 @@ TEST(CharacterTests, characterTest23) {//knight with skill vs everybody(0) witho
     int HPB = Berserk.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 940);
+    EXPECT_EQ(HPB, 890);
+    EXPECT_EQ(HPA, 1090);
 
-    EXPECT_EQ(kb, 20);
-    EXPECT_EQ(ka, 20);
-    EXPECT_EQ(kk, 0);
-    EXPECT_EQ(Kb, 20);
-    EXPECT_EQ(Ka, 20);
-    EXPECT_EQ(Kk, 0);
+    EXPECT_EQ(kb, 55);
+    EXPECT_EQ(ka, 55);
+    EXPECT_EQ(kk, 30);
+    EXPECT_EQ(Kb, 55);
+    EXPECT_EQ(Ka, 55);
+    EXPECT_EQ(Kk, 30);
 }
 
 TEST(CharacterTests, characterTest24) {//knight with skill vs everybody(1) without skill
@@ -492,16 +492,16 @@ TEST(CharacterTests, characterTest24) {//knight with skill vs everybody(1) witho
     int HPB = Berserk.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 970);
+    EXPECT_EQ(HPB, 890);
+    EXPECT_EQ(HPA, 1090);
 
-    EXPECT_EQ(kb, 20);
-    EXPECT_EQ(ka, 20);
-    EXPECT_EQ(kk, 0);
-    EXPECT_EQ(Kb, 20);
-    EXPECT_EQ(Ka, 20);
-    EXPECT_EQ(Kk, 0);
+    EXPECT_EQ(kb, 55);
+    EXPECT_EQ(ka, 55);
+    EXPECT_EQ(kk, 15);
+    EXPECT_EQ(Kb, 55);
+    EXPECT_EQ(Ka, 55);
+    EXPECT_EQ(Kk, 15);
 }
 
 TEST(CharacterTests, characterTest25) {//knight without skill vs everybody(0) with skill
@@ -533,14 +533,14 @@ TEST(CharacterTests, characterTest25) {//knight without skill vs everybody(0) wi
     int HPB = Berserk.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 940);
+    EXPECT_EQ(HPB, 810);
+    EXPECT_EQ(HPA, 1200);
 
-    EXPECT_EQ(kb, 90);
+    EXPECT_EQ(kb, 95);
     EXPECT_EQ(ka, 0);
     EXPECT_EQ(kk, 30);
-    EXPECT_EQ(Kb, 90);
+    EXPECT_EQ(Kb, 95);
     EXPECT_EQ(Ka, 0);
     EXPECT_EQ(Kk, 30);
 }
@@ -574,16 +574,16 @@ TEST(CharacterTests, characterTest26) {//knight without skill vs everybody(1) wi
     int HPB = Berserk.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 970);
+    EXPECT_EQ(HPB, 810);
+    EXPECT_EQ(HPA, 1200);
 
-    EXPECT_EQ(kb, 50);
-    EXPECT_EQ(ka, 50);
-    EXPECT_EQ(kk, 30);
-    EXPECT_EQ(Kb, 50);
-    EXPECT_EQ(Ka, 50);
-    EXPECT_EQ(Kk, 30);
+    EXPECT_EQ(kb, 95);
+    EXPECT_EQ(ka, 0);
+    EXPECT_EQ(kk, 15);
+    EXPECT_EQ(Kb, 95);
+    EXPECT_EQ(Ka, 0);
+    EXPECT_EQ(Kk, 15);
 }
 
 TEST(CharacterTests, characterTest27) {//knight with skill vs everybody(0) with skill
@@ -621,16 +621,16 @@ TEST(CharacterTests, characterTest27) {//knight with skill vs everybody(0) with 
     int HPB = Berserk.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 980);
+    EXPECT_EQ(HPB, 850);
+    EXPECT_EQ(HPA, 1200);
 
-    EXPECT_EQ(kb, 20);
-    EXPECT_EQ(ka, 20);
-    EXPECT_EQ(kk, 0);
-    EXPECT_EQ(Kb, 20);
-    EXPECT_EQ(Ka, 20);
-    EXPECT_EQ(Kk, 0);
+    EXPECT_EQ(kb, 75);
+    EXPECT_EQ(ka, 0);
+    EXPECT_EQ(kk, 10);
+    EXPECT_EQ(Kb, 75);
+    EXPECT_EQ(Ka, 0);
+    EXPECT_EQ(Kk, 10);
 }
 
 TEST(CharacterTests, characterTest28) {//knight with skill vs everybody(1) with skill
@@ -668,16 +668,16 @@ TEST(CharacterTests, characterTest28) {//knight with skill vs everybody(1) with 
     int HPB = Berserk.GetHP();
     int HPA = Assasin.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 990);
+    EXPECT_EQ(HPB, 850);
+    EXPECT_EQ(HPA, 1200);
 
-    EXPECT_EQ(kb, 20);
-    EXPECT_EQ(ka, 20);
-    EXPECT_EQ(kk, 0);
-    EXPECT_EQ(Kb, 20);
-    EXPECT_EQ(Ka, 20);
-    EXPECT_EQ(Kk, 0);
+    EXPECT_EQ(kb, 75);
+    EXPECT_EQ(ka, 0);
+    EXPECT_EQ(kk, 5);
+    EXPECT_EQ(Kb, 75);
+    EXPECT_EQ(Ka, 0);
+    EXPECT_EQ(Kk, 5);
 }
 
 
@@ -708,16 +708,16 @@ TEST(CharacterTests, characterTest31) {//assasin without skill vs everybody(0) w
     int HPB = Berserk.GetHP();
     int HPA = Assasin2.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 850);
+    EXPECT_EQ(HPB, 775);
+    EXPECT_EQ(HPA, 975);
 
-    EXPECT_EQ(ab, 50);
-    EXPECT_EQ(aa, 50);
-    EXPECT_EQ(ak, 30);
-    EXPECT_EQ(Ab, 50);
-    EXPECT_EQ(Aa, 50);
-    EXPECT_EQ(Ak, 30);
+    EXPECT_EQ(ab, 75);
+    EXPECT_EQ(aa, 75);
+    EXPECT_EQ(ak, 50);
+    EXPECT_EQ(Ab, 150);
+    EXPECT_EQ(Aa, 150);
+    EXPECT_EQ(Ak, 100);
 }
 
 TEST(CharacterTests, characterTest32) {//assasin without skill vs everybody(1) without skill
@@ -745,16 +745,16 @@ TEST(CharacterTests, characterTest32) {//assasin without skill vs everybody(1) w
     int HPB = Berserk.GetHP();
     int HPA = Assasin2.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 925);
+    EXPECT_EQ(HPB, 775);
+    EXPECT_EQ(HPA, 975);
 
-    EXPECT_EQ(ab, 50);
-    EXPECT_EQ(aa, 50);
-    EXPECT_EQ(ak, 30);
-    EXPECT_EQ(Ab, 50);
-    EXPECT_EQ(Aa, 50);
-    EXPECT_EQ(Ak, 30);
+    EXPECT_EQ(ab, 75);
+    EXPECT_EQ(aa, 75);
+    EXPECT_EQ(ak, 25);
+    EXPECT_EQ(Ab, 150);
+    EXPECT_EQ(Aa, 150);
+    EXPECT_EQ(Ak, 50);
 }
 
 TEST(CharacterTests, characterTest33) {//assasin with skill vs everybody(0) without skill
@@ -787,16 +787,16 @@ TEST(CharacterTests, characterTest33) {//assasin with skill vs everybody(0) with
     int HPB = Berserk.GetHP();
     int HPA = Assasin2.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 850);
+    EXPECT_EQ(HPB, 775);
+    EXPECT_EQ(HPA, 975);
 
-    EXPECT_EQ(ab, 50);
-    EXPECT_EQ(aa, 50);
-    EXPECT_EQ(ak, 30);
-    EXPECT_EQ(Ab, 50);
-    EXPECT_EQ(Aa, 50);
-    EXPECT_EQ(Ak, 30);
+    EXPECT_EQ(ab, 75);
+    EXPECT_EQ(aa, 75);
+    EXPECT_EQ(ak, 50);
+    EXPECT_EQ(Ab, 150);
+    EXPECT_EQ(Aa, 150);
+    EXPECT_EQ(Ak, 100);
 }
 
 TEST(CharacterTests, characterTest34) {//assasin with skill vs everybody(0) without skill
@@ -829,16 +829,16 @@ TEST(CharacterTests, characterTest34) {//assasin with skill vs everybody(0) with
     int HPB = Berserk.GetHP();
     int HPA = Assasin2.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 925);
+    EXPECT_EQ(HPB, 775);
+    EXPECT_EQ(HPA, 975);
 
-    EXPECT_EQ(ab, 50);
-    EXPECT_EQ(aa, 50);
-    EXPECT_EQ(ak, 30);
-    EXPECT_EQ(Ab, 50);
-    EXPECT_EQ(Aa, 50);
-    EXPECT_EQ(Ak, 30);
+    EXPECT_EQ(ab, 75);
+    EXPECT_EQ(aa, 75);
+    EXPECT_EQ(ak, 25);
+    EXPECT_EQ(Ab, 150);
+    EXPECT_EQ(Aa, 150);
+    EXPECT_EQ(Ak, 50);
 }
 
 TEST(CharacterTests, characterTest35) {//assasin without skill vs everybody(0) with skill
@@ -870,16 +870,16 @@ TEST(CharacterTests, characterTest35) {//assasin without skill vs everybody(0) w
     int HPB = Berserk.GetHP();
     int HPA = Assasin2.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 910);
+    EXPECT_EQ(HPB, 715);
+    EXPECT_EQ(HPA, 1200);
 
-    EXPECT_EQ(ab, 50);
-    EXPECT_EQ(aa, 50);
+    EXPECT_EQ(ab, 95);
+    EXPECT_EQ(aa, 0);
     EXPECT_EQ(ak, 30);
-    EXPECT_EQ(Ab, 50);
-    EXPECT_EQ(Aa, 50);
-    EXPECT_EQ(Ak, 30);
+    EXPECT_EQ(Ab, 190);
+    EXPECT_EQ(Aa, 0);
+    EXPECT_EQ(Ak, 60);
 }
 
 TEST(CharacterTests, characterTest36) {//assasin without skill vs everybody(1) with skill
@@ -911,15 +911,15 @@ TEST(CharacterTests, characterTest36) {//assasin without skill vs everybody(1) w
     int HPB = Berserk.GetHP();
     int HPA = Assasin2.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 955);
+    EXPECT_EQ(HPB, 715);
+    EXPECT_EQ(HPA, 1200);
 
-    EXPECT_EQ(ab, 50);
-    EXPECT_EQ(aa, 50);
-    EXPECT_EQ(ak, 30);
-    EXPECT_EQ(Ab, 50);
-    EXPECT_EQ(Aa, 50);
+    EXPECT_EQ(ab, 95);
+    EXPECT_EQ(aa, 0);
+    EXPECT_EQ(ak, 15);
+    EXPECT_EQ(Ab, 190);
+    EXPECT_EQ(Aa, 0);
     EXPECT_EQ(Ak, 30);
 }
 
@@ -957,16 +957,16 @@ TEST(CharacterTests, characterTest37) {//assasin with skill vs everybody(0) with
     int HPB = Berserk.GetHP();
     int HPA = Assasin2.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 910);
+    EXPECT_EQ(HPB, 715);
+    EXPECT_EQ(HPA, 1200);
 
-    EXPECT_EQ(ab, 50);
-    EXPECT_EQ(aa, 50);
+    EXPECT_EQ(ab, 95);
+    EXPECT_EQ(aa, 0);
     EXPECT_EQ(ak, 30);
-    EXPECT_EQ(Ab, 50);
-    EXPECT_EQ(Aa, 50);
-    EXPECT_EQ(Ak, 30);
+    EXPECT_EQ(Ab, 190);
+    EXPECT_EQ(Aa, 0);
+    EXPECT_EQ(Ak, 60);
 }
 
 TEST(CharacterTests, characterTest38) {//assasin with skill vs everybody(1) with skill
@@ -1003,14 +1003,14 @@ TEST(CharacterTests, characterTest38) {//assasin with skill vs everybody(1) with
     int HPB = Berserk.GetHP();
     int HPA = Assasin2.GetHP();
 
-    EXPECT_EQ(HPK, 335);
-    EXPECT_EQ(HPB, 335);
-    EXPECT_EQ(HPA, 335);
+    EXPECT_EQ(HPK, 955);
+    EXPECT_EQ(HPB, 715);
+    EXPECT_EQ(HPA, 1200);
 
-    EXPECT_EQ(ab, 50);
-    EXPECT_EQ(aa, 50);
-    EXPECT_EQ(ak, 30);
-    EXPECT_EQ(Ab, 50);
-    EXPECT_EQ(Aa, 50);
+    EXPECT_EQ(ab, 95);
+    EXPECT_EQ(aa, 0);
+    EXPECT_EQ(ak, 15);
+    EXPECT_EQ(Ab, 190);
+    EXPECT_EQ(Aa, 0);
     EXPECT_EQ(Ak, 30);
 }
